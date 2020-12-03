dc858d43b43c1b939be4d475d86ea51fbfc8b434
CrxsTagz <mynameiscris000@gmail.com>
2020-12-03 02:30:48 -0400

    Added Bullets and GameObject.cpp and GameObject.hpp to simplify the code

--- Asteroid.hpp
+++ Asteroid.hpp
@@ -3,36 +3,27 @@
 #ifndef ASTEROID_HPP
 #define ASTEROID_HPP
 
-// STL
-#include <vector>
-
+// Asteroids
 #include "Vector2.hpp"
+#include "GameObject.hpp"
 
 namespace Engine
-{
+{    
     class App;
-    class Asteroid
-    {
-    public:
-        Asteroid(App *parent);
-        void Update(float deltaTime);
-        void Render();
-
-    private:
-        /* ==========================
-            * PRIVATE FUNCTIONS
+    class Asteroid : public GameObject
+    {                
+        public:  
+            /* ==========================
+            * PUBLIC FUNCTIONS
             * ==========================*/
-        void ApplyImpulse(Engine::Math::Vector2 impulse);
-        /* ==========================
+            Asteroid(App* parent);
+            void Update(float deltaTime);
+        private:
+            /* ==========================
             * MEMBERS
-            * ==========================*/
-        float m_angle;
-        float m_mass;
-        float m_rotation;
-        Engine::Math::Vector2 m_position;
-        Engine::Math::Vector2 m_velocity;
-        std::vector<Engine::Math::Vector2> m_points;
-        App *m_parent;
+            * ==========================*/            
+            App* m_parent;
     };
-} // namespace Engine
+}
+
 #endif
